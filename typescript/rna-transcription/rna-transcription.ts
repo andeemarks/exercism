1e53d76074c212d4b0058cd795baa7b66f6a50d4
Andy Marks <vampwillow@gmail.com>
2021-05-29 11:11:27 +1000

    Fix of handlebars critical security issue

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,18 +1,23 @@
-const substitutions = llnew Map([['C', 'G'], ['G', 'C'], ['A', 'U'], ['T', 'A']]);
+const substitutions = new Map([
+  ["C", "G"],
+  ["G", "C"],
+  ["A", "U"],
+  ["T", "A"]
+]);
 
 class Transcriptor {
-    transcriber (_: string, letter: string, ): string {
-        let substitution = substitutions.get(letter);
-        if (substitution) {
-            return substitution;
-        }
-
-        throw new Error('Invalid input DNA.');
-      };
-
-    toRna( input: string ): string {
-        return input.replace( /([A-Z])/g, this.transcriber);
+  transcriber = (_: string, letter: string): string => {
+    let substitution = substitutions.get(letter);
+    if (substitution) {
+      return substitution;
     }
-};
 
-export default Transcriptor
+    throw new Error("Invalid input DNA.");
+  };
+
+  toRna(input: string): string {
+    return input.replace(/([A-Z])/g, this.transcriber);
+  }
+}
+
+export default Transcriptor;
